124e85205a8e51d5917c7c8f3532658b87e3b9a5
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2019-06-24 18:25:49 -0300

    adds styling

---
+++ src/styles/colors.js
@@ -0,0 +1,18 @@
+export default {
+  white: '#FFF',
+  lighter: '#EEE',
+  light: '#706e7b',
+  regular: '#999',
+  dark: '#666',
+  darker: '#333',
+  black: '#000',
+
+  primary: '#E5293E',
+  secundary: '#706e7b',
+  success: '#9DCA83',
+  danger: '#e5293e',
+
+  transparent: 'transparent',
+  darkTransparent: 'rgba(0, 0, 0, 0.6)',
+  whiteTransparent: 'rgba(255, 255, 255, 0.3)',
+};
