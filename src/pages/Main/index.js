4d5ce50ddd45589bac623f426018402c98bec606
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2019-06-25 11:09:47 -0300

    adds main page styling

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,57 @@
+/* eslint-disable global-require */
 import React from 'react';
-import { Text, View } from 'react-native';
+import { FlatList, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { colors } from '~/styles';
+import {
+  Container,
+  HeaderContainer,
+  Title,
+  ListContainer,
+  ItemText,
+  CartButton,
+  CartNotificationBadge,
+} from './styles';
+import Layout from '~/components/layouts/MenuLayout';
+
+const data = [
+  'acao1',
+  'acao2',
+  'item3',
+  'item4',
+  'item5',
+  'item6',
+  'item7',
+  'item8',
+  'item9',
+  'item10',
+  'item11',
+  'item12',
+  'item13',
+];
 
 const Main = () => (
-  <View>
-    <Text>Olá pagina main</Text>
-  </View>
+  <Layout source={require('../../../assets/Images/header-background.png')}>
+    <Container>
+      <HeaderContainer>
+        <TouchableOpacity>
+          <Icon name="history" size={24} color={colors.white} />
+        </TouchableOpacity>
+        <Title>Pizzaria Don Juan</Title>
+        <CartButton>
+          <Icon name="shopping" size={24} color={colors.white} />
+          <CartNotificationBadge />
+        </CartButton>
+      </HeaderContainer>
+      <ListContainer>
+        <FlatList
+          data={data}
+          keyExtractor={item => item}
+          renderItem={({ item }) => <ItemText>{item}</ItemText>}
+        />
+      </ListContainer>
+    </Container>
+  </Layout>
 );
 
 export default Main;
---
+++ src/pages/Main/styles.js
@@ -0,0 +1,56 @@
+import styled from 'styled-components/native';
+import { colors, metrics } from '~/styles';
+import { getStatusBarHeight } from 'react-native-status-bar-height';
+
+export const Container = styled.View`
+  flex-direction: column;
+  align-items: stretch;
+  flex: 1;
+  padding: ${getStatusBarHeight()}px ${metrics.basePadding}px;
+`;
+
+export const HeaderContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  margin: ${metrics.basePadding}px 0;
+`;
+
+export const Title = styled.Text`
+  font-family: Helvetica;
+  font-size: 18px;
+  color: #ffffff;
+  letter-spacing: 0;
+  text-align: left;
+  font-weight: bold;
+`;
+
+export const ListContainer = styled.View``;
+
+export const ItemText = styled.Text`
+  font-family: Helvetica;
+  font-size: 18px;
+  color: #ffffff;
+  letter-spacing: 0;
+  text-align: center;
+  font-weight: bold;
+`;
+
+export const CartButton = styled.TouchableOpacity`
+  height: 35px;
+  width: 35px;
+  border-radius: 15px;
+  align-items: center;
+  justify-content: center;
+  background: ${colors.primary};
+  position: relative;
+`;
+
+export const CartNotificationBadge = styled.View`
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 10px;
+  width: 10px;
+  border-radius: 5px;
+  background-color: ${colors.yellow};
+`;
