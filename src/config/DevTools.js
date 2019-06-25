c75002ab7422582e008049441fa29eecd1f6cdd0
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2019-06-24 22:25:36 -0300

    removes anoying remote debugger warning

--- src/config/DevTools.js
+++ src/config/DevTools.js
@@ -1,4 +1,7 @@
+import { YellowBox } from 'react-native';
+
 if (__DEV__) {
   // eslint-disable-next-line global-require
   require('react-devtools');
+  YellowBox.ignoreWarnings(['Remote debugger']);
 }
