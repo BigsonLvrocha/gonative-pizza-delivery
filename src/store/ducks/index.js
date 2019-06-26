9111c44d51a7c73cf64d6af0621b3b59b341a627
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2019-06-26 00:15:45 -0300

    adds type reducer to store

--- src/store/ducks/index.js
+++ src/store/ducks/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import session from './session';
 import signup from './signup';
 import product from './product';
+import type from './type';
 
 export default combineReducers({
   session,
   signup,
   product,
+  type,
 });
