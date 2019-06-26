6dcd0e518a707eec804781faa517d54ab5a4be49
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2019-06-26 10:50:20 -0300

    adds header and navigation for size page

--- src/pages/Size/index.js
+++ src/pages/Size/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import Layout from '~/components/layouts/MenuLayout';
-
-import { Text } from 'react-native';
-
+import { TouchableOpacity } from 'react-native';
+import PropTypes from 'prop-types';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { colors } from '~/styles';
+import { Container, HeaderContainer, PageTitle } from './styles';
 // import { Container } from './styles';
 
-const Size = () => (
+const Size = ({ navigation }) => (
   <Layout>
-    <Text>hello from Size page</Text>
+    <Container>
+      <HeaderContainer>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Icon name="chevron-left" size={20} color={colors.white} />
+        </TouchableOpacity>
+        <PageTitle>Selecione um tipo</PageTitle>
+      </HeaderContainer>
+    </Container>
   </Layout>
 );
+
+Size.propTypes = {
+  navigation: PropTypes.shape({
+    goBack: PropTypes.func.isRequired,
+    getParam: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Size;
---
+++ src/pages/Size/styles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+import { getStatusBarHeight } from 'react-native-status-bar-height';
+import { metrics } from '~/styles';
+
+export const Container = styled.View`
+  flex-direction: column;
+  align-items: stretch;
+  flex: 1;
+  padding: ${getStatusBarHeight()}px 0;
+`;
+
+export const HeaderContainer = styled.View`
+  flex-direction: row;
+  justify-content: flex-start;
+  margin: ${metrics.basePadding}px ${metrics.basePadding}px;
+  align-items: center;
+`;
+
+export const PageTitle = styled.Text`
+  font-family: Helvetica;
+  font-size: 18px;
+  color: #ffffff;
+  letter-spacing: 0;
+  text-align: left;
+  font-weight: bold;
+  padding-left: ${metrics.baseMargin};
+`;
