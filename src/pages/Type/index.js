cd7d1d2be0f2d9cbd69835125ee58c52632de6e7
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2019-06-25 23:03:02 -0300

    adds back button for types

--- src/pages/Type/index.js
+++ src/pages/Type/index.js
@@ -1,19 +1,28 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 import Layout from '~/components/layouts/MenuLayout';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import PropTypes from 'prop-types';
 import { colors } from '~/styles';
 import { Container, HeaderContainer, PageTitle } from './styles';
 
-const Type = () => (
+const Type = ({ navigation }) => (
   <Layout>
     <Container>
       <HeaderContainer>
-        <Icon name="chevron-left" size={20} color={colors.white} />
+        <TouchableOpacity onPress={() => navigation.navigate('Main')}>
+          <Icon name="chevron-left" size={20} color={colors.white} />
+        </TouchableOpacity>
         <PageTitle>Selecione um tipo</PageTitle>
       </HeaderContainer>
     </Container>
   </Layout>
 );
+
+Type.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Type;
