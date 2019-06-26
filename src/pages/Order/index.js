d11f5bb94711614d3608918112e236d828a56466
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2019-06-26 17:17:54 -0300

    adds header to order page with back navigation

--- src/pages/Order/index.js
+++ src/pages/Order/index.js
@@ -1,14 +1,38 @@
 import React from 'react';
-
-import { View, Text } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import Layout from '~/components/layouts/MenuLayout';
 
-// import { Container } from './styles';
+import {
+  Container, HeaderContainer, Title, PriceTag,
+} from './styles';
+import { colors } from '~/styles';
 
-const Order = () => (
+const Order = ({ total, navigation }) => (
   <Layout>
-    <Text>Hello from Order page</Text>
+    <Container>
+      <HeaderContainer>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Icon name="chevron-left" size={20} color={colors.white} />
+        </TouchableOpacity>
+        <Title>Complete seu pedido</Title>
+        <PriceTag>{total.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</PriceTag>
+      </HeaderContainer>
+    </Container>
   </Layout>
 );
 
-export default Order;
+Order.propTypes = {
+  total: PropTypes.number.isRequired,
+  navigation: PropTypes.shape({
+    goBack: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+const mapStateToProps = state => ({
+  total: state.cart.items.reduce((acc, actual) => acc + actual.item.price, 0),
+});
+
+export default connect(mapStateToProps)(Order);
