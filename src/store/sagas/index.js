7a363ac36a4f90f4ab762a72159ad926215baf79
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2019-06-26 18:48:50 -0300

    adds cep saga to root saga

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -4,11 +4,13 @@ import * as SignupSagas from './signup';
 import * as ProductSagas from './product';
 import * as TypeSagas from './type';
 import * as SizeSagas from './size';
+import * as CepSagas from './cep';
 import { Types as SessionTypes } from '~/store/ducks/session';
 import { Types as SignupTypes } from '~/store/ducks/signup';
 import { Types as ProductTypes } from '~/store/ducks/product';
 import { Types as TypeTypes } from '~/store/ducks/type';
 import { Types as SizeTypes } from '~/store/ducks/size';
+import { Types as CepTypes } from '~/store/ducks/cep';
 
 export default function* rootSaga() {
   return yield all([
@@ -18,5 +20,6 @@ export default function* rootSaga() {
     takeLatest(ProductTypes.FETCH_PRODUCTS_REQUEST, ProductSagas.fetchProductRequest),
     takeLatest(TypeTypes.FETCH_TYPES_REQUEST, TypeSagas.fetchTypeRequest),
     takeLatest(SizeTypes.FETCH_SIZES_REQUEST, SizeSagas.fetchSizesRequest),
+    takeLatest(CepTypes.FETCH_CEP_REQUEST, CepSagas.fetchCepRequest),
   ]);
 }
