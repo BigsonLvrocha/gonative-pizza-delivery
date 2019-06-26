34e557eb4e94bde204b237476998c38231d3088d
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2019-06-25 22:04:58 -0300

    adds product duck to store

--- src/store/ducks/index.js
+++ src/store/ducks/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import session from './session';
 import signup from './signup';
+import product from './product';
 
 export default combineReducers({
   session,
   signup,
+  product,
 });
