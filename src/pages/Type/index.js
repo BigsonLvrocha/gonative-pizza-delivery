4a47b89d026f64f2a2e5bf96c31062ec97541a3f
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2019-06-25 22:59:50 -0300

    adds header

--- src/pages/Type/index.js
+++ src/pages/Type/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import Layout from '~/components/layouts/MenuLayout';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { colors } from '~/styles';
+import { Container, HeaderContainer, PageTitle } from './styles';
 
 const Type = () => (
   <Layout>
-    <View>
-      <Text>Olá pagina de tipos</Text>
-    </View>
+    <Container>
+      <HeaderContainer>
+        <Icon name="chevron-left" size={20} color={colors.white} />
+        <PageTitle>Selecione um tipo</PageTitle>
+      </HeaderContainer>
+    </Container>
   </Layout>
 );
 
