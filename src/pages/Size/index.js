36952dc9713890beb22cf8b59ee5ab16ff803738
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2019-06-26 14:32:46 -0300

    adds funcionality to sizes page

--- src/pages/Size/index.js
+++ src/pages/Size/index.js
@@ -1,45 +1,80 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Layout from '~/components/layouts/MenuLayout';
-import { TouchableOpacity, FlatList } from 'react-native';
+import { TouchableOpacity, FlatList, ActivityIndicator } from 'react-native';
 import PropTypes from 'prop-types';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Creators as SizeActions } from '~/store/ducks/size';
 import { colors } from '~/styles';
 import {
-  Container, HeaderContainer, PageTitle, ListContainer,
+  Container, HeaderContainer, PageTitle, ListContainer, ErrorText,
 } from './styles';
 import SizeItem from './SizeItem';
-import data from './mockData';
 
 // import { Container } from './styles';
 
-const Size = ({ navigation }) => (
-  <Layout>
-    <Container>
-      <HeaderContainer>
-        <TouchableOpacity onPress={() => navigation.goBack()}>
-          <Icon name="chevron-left" size={20} color={colors.white} />
-        </TouchableOpacity>
-        <PageTitle>Selecione um tamanho</PageTitle>
-      </HeaderContainer>
-      <ListContainer>
-        <FlatList
-          data={data}
-          keyExtractor={item => String(item.id)}
-          renderItem={({ item }) => (
-            <SizeItem item={item} productId={navigation.getParam('productId')} />
-          )}
-          numColumns={2}
-        />
-      </ListContainer>
-    </Container>
-  </Layout>
-);
+class Size extends Component {
+  static propTypes = {
+    navigation: PropTypes.shape({
+      goBack: PropTypes.func.isRequired,
+      getParam: PropTypes.func.isRequired,
+    }).isRequired,
+    fetchSizesRequest: PropTypes.func.isRequired,
+    size: PropTypes.shape({
+      data: PropTypes.array.isRequired,
+      error: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]).isRequired,
+      isLoading: PropTypes.bool.isRequired,
+    }).isRequired,
+  };
 
-Size.propTypes = {
-  navigation: PropTypes.shape({
-    goBack: PropTypes.func.isRequired,
-    getParam: PropTypes.func.isRequired,
-  }).isRequired,
-};
+  componentDidMount() {
+    const { navigation, fetchSizesRequest } = this.props;
+    const productId = navigation.getParam('productId');
+    const typeId = navigation.getParam('typeId');
+    fetchSizesRequest(productId, typeId);
+  }
 
-export default Size;
+  render() {
+    const {
+      size: { data, error, isLoading },
+      navigation,
+    } = this.props;
+    return (
+      <Layout>
+        <Container>
+          <HeaderContainer>
+            <TouchableOpacity onPress={() => navigation.goBack()}>
+              <Icon name="chevron-left" size={20} color={colors.white} />
+            </TouchableOpacity>
+            <PageTitle>Selecione um tamanho</PageTitle>
+          </HeaderContainer>
+          <ListContainer>
+            {error && <ErrorText>{error}</ErrorText>}
+            {isLoading ? (
+              <ActivityIndicator size="large" color={colors.white} />
+            ) : (
+              <FlatList
+                data={data}
+                keyExtractor={item => String(item.id)}
+                renderItem={({ item }) => <SizeItem item={item} />}
+                numColumns={2}
+              />
+            )}
+          </ListContainer>
+        </Container>
+      </Layout>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  size: state.size,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(SizeActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Size);
