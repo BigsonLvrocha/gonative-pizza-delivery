7aa4b0d841ad5b5e97f45484c71e38fa8d0e2ce5
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2019-06-26 20:52:26 -0300

    adds header to profiles page

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import PropTypes from 'prop-types';
+import Layout from '~/components/layouts/MenuLayout';
+import { Container, PageTitle, HeaderContainer } from './styles';
+import { colors } from '~/styles';
 
-import { View, Text } from 'react-native';
-
-// import { Container } from './styles';
-
-const Profile = () => (
-  <View>
-    <Text>Hello from profile page</Text>
-  </View>
+const Profile = ({ navigation }) => (
+  <Layout>
+    <Container>
+      <HeaderContainer>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Icon size={20} color={colors.white} name="chevron-left" />
+        </TouchableOpacity>
+        <PageTitle>Meus pedidos</PageTitle>
+      </HeaderContainer>
+    </Container>
+  </Layout>
 );
+
+Profile.propTypes = {
+  navigation: PropTypes.shape({
+    goBack: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Profile;
