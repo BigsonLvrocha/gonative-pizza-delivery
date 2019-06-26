269a642c05e6eed42c5d0892995b5e7c1a52e3e3
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2019-06-26 19:59:32 -0300

    adds cart saga

---
+++ src/store/sagas/cart.js
@@ -0,0 +1,34 @@
+import { call, put, select } from 'redux-saga/effects';
+import { AppApi } from '~/services/api';
+import { Creators as CartActions } from '~/store/ducks/cart';
+
+export function* placeOrderRequest({
+  payload: {
+    observations, number, street, bairro, cep,
+  },
+}) {
+  const { items } = yield select(state => state.cart);
+  const { loggedUserToken } = yield select(state => state.session);
+  const sizes = items.map(item => item.item.id);
+  try {
+    yield call(
+      AppApi.post,
+      '/user/order',
+      {
+        observations,
+        number,
+        street,
+        cep,
+        district: bairro,
+        sizes,
+      },
+      {
+        headers: {
+          Authorization: `bearer ${loggedUserToken}`,
+        },
+      },
+    );
+  } catch (e) {
+    yield put(CartActions.placeOrderFailure('Não foi possível registrar o pedido'));
+  }
+}
