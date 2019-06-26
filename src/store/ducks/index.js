41330c78bf0f7f7ebf36dc7c60048142bcce74f3
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2019-06-26 14:20:19 -0300

    adds size duck to store

--- src/store/ducks/index.js
+++ src/store/ducks/index.js
@@ -3,10 +3,12 @@ import session from './session';
 import signup from './signup';
 import product from './product';
 import type from './type';
+import size from './size';
 
 export default combineReducers({
   session,
   signup,
   product,
   type,
+  size,
 });
