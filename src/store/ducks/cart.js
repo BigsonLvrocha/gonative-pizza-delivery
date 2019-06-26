3f4c246b8072e2b147993513397933ee94f49646
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2019-06-26 19:44:01 -0300

    increments cart duck to send order

--- src/store/ducks/cart.js
+++ src/store/ducks/cart.js
@@ -2,11 +2,17 @@
 export const Types = {
   ADD_ITEM: 'cart/ADD_ITEM',
   REMOVE_ITEM: 'cart/REMOVE_ITEM',
+  CLEAR_CART: 'cart/CLEAR_CART',
+  PLACE_ORDER_REQUEST: 'cart/PLACE_ORDER_REQUEST',
+  PLACE_ORDER_SUCCESS: 'cart/PLACE_ORDER_SUCCESS',
+  PLACE_ORDER_FAILURE: 'cart/PLACE_ORDER_FAILURE',
 };
 
 // reducer
 const INITIAL_STATE = {
   items: [],
+  isLoading: false,
+  error: false,
 };
 
 export default function reducer(state = INITIAL_STATE, { type, payload }) {
@@ -29,6 +35,29 @@ export default function reducer(state = INITIAL_STATE, { type, payload }) {
         ...state,
         items: state.items.filter(item => item.id !== payload.id),
       };
+    case Types.CLEAR_CART:
+      return {
+        ...state,
+        items: [],
+      };
+    case Types.PLACE_ORDER_REQUEST:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case Types.PLACE_ORDER_SUCCESS:
+      return {
+        ...state,
+        items: [],
+        error: false,
+        isLoading: false,
+      };
+    case Types.PLACE_ORDER_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        error: payload.error,
+      };
     default:
       return state;
   }
@@ -46,6 +75,28 @@ export const Creators = {
     type: Types.REMOVE_ITEM,
     payload: {
       id,
+    },
+  }),
+  clearCart: () => ({
+    type: Types.CLEAR_CART,
+  }),
+  placeOrderRequest: (cep, street, number, bairro, observations) => ({
+    type: Types.PLACE_ORDER_REQUEST,
+    payload: {
+      cep,
+      street,
+      number,
+      bairro,
+      observations,
+    },
+  }),
+  placeOrderSuccess: () => ({
+    type: Types.PLACE_ORDER_SUCCESS,
+  }),
+  placeOrderFailure: error => ({
+    type: Types.PLACE_ORDER_FAILURE,
+    payload: {
+      error,
     },
   }),
 };
