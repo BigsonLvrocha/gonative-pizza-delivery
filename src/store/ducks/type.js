16debfe25a2e7aeb56eb68b714d0409743f04a39
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2019-06-26 00:13:32 -0300

    adds type duck

---
+++ src/store/ducks/type.js
@@ -0,0 +1,70 @@
+// types
+export const Types = {
+  FETCH_TYPES_REQUEST: 'type/FETCH_TYPES_REQUEST',
+  FETCH_TYPES_SUCCESS: 'type/FETCH_TYPES_SUCCESS',
+  FETCH_TYPES_FAILURE: 'type/FETCH_TYPES_FAILURE',
+  CLEAR_ERRORS: 'type/CLEAR_ERRORS',
+};
+
+// reducers
+const INITIAL_STATE = {
+  isLoading: false,
+  data: [],
+  error: false,
+};
+
+export default function reducer(state = INITIAL_STATE, { type, payload }) {
+  switch (type) {
+    case Types.FETCH_TYPES_REQUEST:
+      return {
+        ...state,
+        isLoading: true,
+        data: [],
+      };
+    case Types.FETCH_TYPES_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        data: payload.data,
+        error: false,
+      };
+    case Types.FETCH_TYPES_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        error: payload.error,
+      };
+    case Types.CLEAR_ERRORS:
+      return {
+        ...state,
+        error: false,
+      };
+    default:
+      return state;
+  }
+}
+
+// action creators
+export const Creators = {
+  fetchTypesRequest: productId => ({
+    type: Types.FETCH_TYPES_REQUEST,
+    payload: {
+      productId,
+    },
+  }),
+  fetchTypesSuccess: data => ({
+    type: Types.FETCH_TYPES_SUCCESS,
+    payload: {
+      data,
+    },
+  }),
+  fetchTypesFailure: error => ({
+    type: Types.FETCH_TYPES_FAILURE,
+    payload: {
+      error,
+    },
+  }),
+  clearErrors: () => ({
+    type: Types.CLEAR_ERRORS,
+  }),
+};
