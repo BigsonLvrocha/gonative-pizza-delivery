f3b671d9ad79e9fad8234fac298222b5cecbaaa9
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2019-06-26 14:46:11 -0300

    adds heade styling

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
-
-import { View, Text } from 'react-native';
-
-// import { Container } from './styles';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import Layout from '~/components/layouts/MenuLayout';
+import {
+  Container, HeaderContainer, Title, PriceTag,
+} from './styles';
+import { colors } from '~/styles';
 
 const Cart = () => (
-  <View>
-    <Text>Hello from cart page</Text>
-  </View>
+  <Layout>
+    <Container>
+      <HeaderContainer>
+        <TouchableOpacity>
+          <Icon name="chevron-left" size={24} color={colors.white} />
+        </TouchableOpacity>
+        <Title>Carrinho</Title>
+        <PriceTag>R$ 30,00</PriceTag>
+      </HeaderContainer>
+    </Container>
+  </Layout>
 );
 
 export default Cart;
---
+++ src/pages/Cart/styles.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components/native';
+import { getStatusBarHeight } from 'react-native-status-bar-height';
+import { metrics, colors } from '~/styles';
+
+export const Container = styled.View`
+  flex-direction: column;
+  align-items: stretch;
+  flex: 1;
+  padding: ${getStatusBarHeight()}px 0;
+`;
+
+export const HeaderContainer = styled.View`
+  margin: ${metrics.basePadding}px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  font-family: Helvetica;
+  font-size: 18px;
+  color: #ffffff;
+  letter-spacing: 0;
+  text-align: left;
+  font-weight: bold;
+  flex: 1;
+`;
+
+export const PriceTag = styled.Text`
+  font-family: Helvetica;
+  font-size: 16px;
+  color: #ffffff;
+  letter-spacing: 0;
+  text-align: right;
+  font-weight: bold;
+`;
