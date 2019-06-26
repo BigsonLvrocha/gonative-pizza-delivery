caa5fb62b4649bddb4f3d7665cc7d59cf790c385
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2019-06-26 15:27:46 -0300

    adds initial stilyting of cart page

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,24 +1,50 @@
 import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity, FlatList, Text } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Layout from '~/components/layouts/MenuLayout';
 import {
-  Container, HeaderContainer, Title, PriceTag,
+  Container,
+  HeaderContainer,
+  Title,
+  PriceTag,
+  ActionsContainer,
+  AddCartButton,
+  PlaceOrderButton,
+  PlaceOrderText,
 } from './styles';
 import { colors } from '~/styles';
 
-const Cart = ({ total, navigation }) => (
+const Cart = ({ total, navigation, items }) => (
   <Layout>
     <Container>
       <HeaderContainer>
         <TouchableOpacity onPress={() => navigation.goBack()}>
-          <Icon name="chevron-left" size={24} color={colors.white} />
+          <Icon name="chevron-left" size={20} color={colors.white} />
         </TouchableOpacity>
         <Title>Carrinho</Title>
         <PriceTag>{total.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</PriceTag>
       </HeaderContainer>
+      <FlatList
+        data={items}
+        keyExtractor={item => String(item.id)}
+        renderItem={({ item }) => <Text>{item.item.productType.cart_name}</Text>}
+      />
+      <ActionsContainer>
+        <AddCartButton>
+          <Icon name="cart-plus" color={colors.darker} size={20} />
+        </AddCartButton>
+        <PlaceOrderButton>
+          <PlaceOrderText>REALIZAR PEDIDO</PlaceOrderText>
+          <Icon
+            name="chevron-right"
+            size={20}
+            color={colors.white}
+            style={{ textAlign: 'center' }}
+          />
+        </PlaceOrderButton>
+      </ActionsContainer>
     </Container>
   </Layout>
 );
@@ -28,9 +54,16 @@ Cart.propTypes = {
   navigation: PropTypes.shape({
     goBack: PropTypes.func.isRequired,
   }).isRequired,
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      item: PropTypes.shape({}).isRequired,
+    }),
+  ).isRequired,
 };
 
 const mapStateToProps = state => ({
+  items: state.cart.items,
   total: state.cart.items.reduce((acc, actual) => acc + actual.item.price, 0),
 });
 
--- src/pages/Cart/styles.js
+++ src/pages/Cart/styles.js
@@ -34,3 +34,40 @@ export const PriceTag = styled.Text`
   text-align: right;
   font-weight: bold;
 `;
+
+export const ActionsContainer = styled.View`
+  margin-top: ${metrics.baseMargin}px;
+  padding: 0 ${metrics.basePadding}px;
+  justify-content: space-between;
+  align-content: center;
+  flex-direction: row;
+`;
+
+export const AddCartButton = styled.TouchableOpacity`
+  background: ${colors.regular};
+  align-content: center;
+  justify-content: center;
+  width: 32px;
+  height: 32px;
+  border-radius: 16px;
+  padding: 6px;
+`;
+
+export const PlaceOrderButton = styled.TouchableOpacity`
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 8px ${metrics.basePadding}px;
+  align-content: center;
+  border-radius: 16px;
+  background: ${colors.primary};
+`;
+
+export const PlaceOrderText = styled.Text`
+  font-family: Helvetica;
+  font-size: 14px;
+  color: #ffffff;
+  letter-spacing: 0;
+  text-align: center;
+  font-weight: bold;
+  margin-right: ${metrics.baseMargin}px;
+`;
