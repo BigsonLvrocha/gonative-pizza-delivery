d85195d0e526a4460f08b535b8ad759a14c943ec
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2019-06-26 14:24:11 -0300

    adds size saga to root saga

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -3,10 +3,12 @@ import * as SessionSagas from './session';
 import * as SignupSagas from './signup';
 import * as ProductSagas from './product';
 import * as TypeSagas from './type';
+import * as SizeSagas from './size';
 import { Types as SessionTypes } from '~/store/ducks/session';
 import { Types as SignupTypes } from '~/store/ducks/signup';
 import { Types as ProductTypes } from '~/store/ducks/product';
 import { Types as TypeTypes } from '~/store/ducks/type';
+import { Types as SizeTypes } from '~/store/ducks/size';
 
 export default function* rootSaga() {
   return yield all([
@@ -15,5 +17,6 @@ export default function* rootSaga() {
     takeLatest(SignupTypes.CREATE_USER_REQUEST, SignupSagas.createUserRequest),
     takeLatest(ProductTypes.FETCH_PRODUCTS_REQUEST, ProductSagas.fetchProductRequest),
     takeLatest(TypeTypes.FETCH_TYPES_REQUEST, TypeSagas.fetchTypeRequest),
+    takeLatest(SizeTypes.FETCH_SIZES_REQUEST, SizeSagas.fetchSizesRequest),
   ]);
 }
--- src/store/sagas/size.js
+++ src/store/sagas/size.js
@@ -2,7 +2,7 @@ import { call, put, select } from 'redux-saga/effects';
 import { AppApi } from '~/services/api';
 import { Creators as SizeActions } from '~/store/ducks/size';
 
-export function* fetchTypeRequest({ payload: { productId, typeId } }) {
+export function* fetchSizesRequest({ payload: { productId, typeId } }) {
   try {
     const { loggedUserToken } = yield select(state => state.session);
     const { data } = yield call(AppApi.get, `/product/${productId}/type/${typeId}/size`, {
