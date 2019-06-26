5979bca2a04ac15f3af17e9d859965dd5a468333
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2019-06-26 00:17:42 -0300

    adds Type saga to root saga

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -2,9 +2,11 @@ import { all, takeLatest } from 'redux-saga/effects';
 import * as SessionSagas from './session';
 import * as SignupSagas from './signup';
 import * as ProductSagas from './product';
+import * as TypeSagas from './type';
 import { Types as SessionTypes } from '~/store/ducks/session';
 import { Types as SignupTypes } from '~/store/ducks/signup';
 import { Types as ProductTypes } from '~/store/ducks/product';
+import { Types as TypeTypes } from '~/store/ducks/type';
 
 export default function* rootSaga() {
   return yield all([
@@ -12,5 +14,6 @@ export default function* rootSaga() {
     takeLatest(SessionTypes.FETCH_LOGGED_USER_REQUEST, SessionSagas.fetchLoggedUserRequest),
     takeLatest(SignupTypes.CREATE_USER_REQUEST, SignupSagas.createUserRequest),
     takeLatest(ProductTypes.FETCH_PRODUCTS_REQUEST, ProductSagas.fetchProductRequest),
+    takeLatest(TypeTypes.FETCH_TYPES_REQUEST, TypeSagas.fetchTypeRequest),
   ]);
 }
